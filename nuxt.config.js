ca04443e60d88f58d75be457ab45a19e008027d6
Justin B. Greenlee <justin.greenlee@rewardstyle.com>
2021-02-24 17:08:20 -0600

    :bug: fixed vercel master branch deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,7 +39,8 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    '@nuxt/content'
+    '@nuxt/content',
+    '@nuxtjs/eslint-module',
   ],
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
