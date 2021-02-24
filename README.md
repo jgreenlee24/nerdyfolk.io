2d27d313832a3abf828d4391d24f501f514d0a29
Justin B. Greenlee <justin.greenlee@rewardstyle.com>
2021-02-24 16:54:08 -0600

    init project! :tada:

---
+++ README.md
@@ -0,0 +1,24 @@
+# nerdyfolk.io
+
+## Build Setup
+
+```bash
+# install dependencies
+$ npm install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm run start
+
+# generate static project
+$ npm run generate
+```
+
+For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
+
+## Tutorials
+* [How to How NuxtJS SSR Universal Application](https://dev.to/fayaz/what-is-nuxtjs-ssr-universal-mode-and-how-to-host-it-for-free-3km4)
+* [How to Build a Blog with NuxtJS](https://devdojo.com/mezie/build-a-blog-with-nuxtjs)
---
+++ pages/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <section class="hero is-primary has-text-centered">
+      <div class="hero-body">
+        <div class="container">
+          <h2 class="subtitle">
+            Welcome to nerdyfolk, the social hotspot for all things geekdom.
+            Are you a non-profit in need a new, up-to-standard website? Shoot us an email at 
+            <a href = "mailto: jgreenlee24@gmail.com">jgreenlee24@gmail.com</a>
+          </h2>
+        </div>
+      </div>
+    </section>
+
+    <section class="section">
+      <div class="container">
+        <div class="columns">
+          <div class="column is-three-fifths is-offset-one-fifth">
+            <div v-for="post in posts" :key="post.slug" class="mb-5">
+              <h3 class="title is-4">
+                <nuxt-link :to="post.slug">
+                  {{ post.title }}
+                </nuxt-link>
+              </h3>
+              <div>
+                {{ post.description }}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData ({ $content }) {
+    const posts = await $content('posts').sortBy('createdAt', 'desc').fetch()
+    return { posts }
+  }
+}
+</script>
+
+<style>
+  h3 a {
+    color: #1E2021;
+  }
+  h3 a:hover {
+    color: #3273dc;
+  }
+</style>
